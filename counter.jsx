620b7645d560c8c2a08d4204b4296c9f26cc542b
NITIN GUPTA <SUPERFULL03213@GMAIL.COM>
2026-05-21 21:27:13 +0530

    Create counter.jsx

---
+++ counter.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="card">
+      <h2>Counter App</h2>
+
+      <h1>{count}</h1>
+
+      <div className="buttons">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
